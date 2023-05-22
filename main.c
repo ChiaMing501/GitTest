5593533b76f25bbd279313ff66df0c9d58c6480a
stanleychang <homicide522@gmail.com>
2023-05-22 16:43:49 +0800

    Add a function showUserMessage()

--- main.c
+++ main.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
+void showUserMessage(const char *msg);
+
 int main(int argc, char *argv[])
 {
     printf("Hello World\n");
+    
+    showUserMessage("I am king of the world");
 
     return 0;
 
 } //end of function main
+
+void showUserMessage(const char *msg)
+{
+	printf("Message: %s\n", msg);
+	
+} //end of function showUserMessage
